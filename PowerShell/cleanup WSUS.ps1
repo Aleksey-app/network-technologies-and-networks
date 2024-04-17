664e6f365674afa62052d6e1996b4a5e699c2ffa
Aleksey <71906604+Aleksey-app@users.noreply.github.com>
2024-04-17 18:22:22 +0000

    ok

---
+++ PowerShell/cleanup WSUS.ps1
@@ -0,0 +1,35 @@
+# Подключаем модуль WSUS
+Import-Module UpdateServices
+
+# Подключаемся к серверу WSUS
+$wsusServer = "имя_сервера_вашего_WSUS"
+$wsus = Get-WsusServer -Name $wsusServer
+
+# Создаем каталог для журнала выполнения
+$logDirectory = "D:\systemWSUS"
+New-Item -Path $logDirectory -ItemType Directory -Force | Out-Null
+
+# Задаем путь к файлу журнала выполнения
+$logPath = Join-Path -Path $logDirectory -ChildPath "WSUS_Cleanup_Log_$(Get-Date -Format 'yyyyMMdd_HHmmss').txt"
+
+# Направляем вывод в файл журнала выполнения
+Start-Transcript -Path $logPath
+
+# Удаляем неиспользуемые обновления из базы данных WSUS
+Write-Host "Удаление неиспользуемых обновлений из базы данных WSUS..."
+$wsus.CleanupObsoleteUpdates()
+
+# Удаляем неиспользуемые компьютеры из базы данных WSUS
+Write-Host "Удаление неиспользуемых компьютеров из базы данных WSUS..."
+$wsus.CleanupObsoleteComputers()
+
+# Оптимизируем базу данных WSUS
+Write-Host "Оптимизация базы данных WSUS..."
+$wsus.Runspace.PerformingCleanup = $true
+$wsus.Runspace.PerformCleanup()
+$wsus.Runspace.PerformingCleanup = $false
+
+Write-Host "Очистка базы данных WSUS завершена."
+
+# Останавливаем запись в файл журнала выполнения
+Stop-Transcript
