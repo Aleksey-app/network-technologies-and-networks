1e8e6ebba1c97ff60fc4c395cb1fdb3a1e037d46
Aleksey <71906604+Aleksey-app@users.noreply.github.com>
2024-04-17 18:26:28 +0000

    Update Free disk space on the remote computer.ps1

--- PowerShell/Free disk space on the remote computer.ps1
+++ PowerShell/Free disk space on the remote computer.ps1
@@ -1,4 +1,4 @@
-﻿Get-WmiObject -Class Win32_LogicalDisk -ComputerName na...me |
+﻿Get-WmiObject -Class Win32_LogicalDisk -ComputerName "имя_рабочей_станции" |
 Select-Object -Property DeviceID, VolumeName, @{Label='Свободно (Gb)'; expression={($_.FreeSpace/1GB).ToString('F2')}},
 @{Label='Всего (Gb)'; expression={($_.Size/1GB).ToString('F2')}},
 @{label='Свободно %'; expression={[Math]::Round(($_.freespace / $_.size) * 100, 2).ToString('F2')+'%'}}|ft
