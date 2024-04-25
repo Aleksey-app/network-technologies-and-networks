952b0352abd01184282af2eb5ac8a7c51baf5a73
Aleksey <71906604+Aleksey-app@users.noreply.github.com>
2024-04-25 19:48:53 +0000

    Update pingIP.ps1

--- PowerShell/pingIP.ps1
+++ PowerShell/pingIP.ps1
@@ -1,30 +1,28 @@
-# Функция для выполнения пинг-проверки
+# Функция для выполнения пинг-проверки с записью в лог
 function Test-IPRange {
     param (
         [string]$StartIP,
-        [string]$EndIP
+        [string]$EndIP,
+        [string]$LogFile
     )
-
     $ip = [System.Net.IPAddress]::Parse($StartIP)
     $endIP = [System.Net.IPAddress]::Parse($EndIP)
 
     while ($ip.Address -le $endIP.Address) {
         $ipStr = $ip.ToString()
         $result = Test-Connection -ComputerName $ipStr -Count 1 -Quiet
-
         if ($result) {
-            Write-Host "$ipStr is reachable"
+            "$ipStr is reachable" | Out-File -FilePath $LogFile -Append
         } else {
-            Write-Host "$ipStr is unreachable"
+            "$ipStr is unreachable" | Out-File -FilePath $LogFile -Append
         }
-
         $ip = [System.Net.IPAddress]::Parse($ip.Address + 1)
     }
 }
-
 # Указать начальный и конечный IP-адреса диапазона
 $StartIP = "192.168.0.1"
 $EndIP = "192.168.0.10"
+$LogFile = "ping_log.txt"
 
 # Вызов функции для проверки диапазона
-Test-IPRange $StartIP $EndIP
+Test-IPRange $StartIP $EndIP $LogFile
