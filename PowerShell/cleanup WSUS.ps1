5f47b918b5a3d07f12490155e76da5812fc5ec26
Aleksey <71906604+Aleksey-app@users.noreply.github.com>
2024-04-30 19:10:18 +0000

    Update cleanup WSUS.ps1

--- PowerShell/cleanup WSUS.ps1
+++ PowerShell/cleanup WSUS.ps1
@@ -5,31 +5,36 @@ Import-Module UpdateServices
 $wsusServer = "имя_сервера_вашего_WSUS"
 $wsus = Get-WsusServer -Name $wsusServer
 
-# Создаем каталог для журнала выполнения
-$logDirectory = "D:\systemWSUS"
-New-Item -Path $logDirectory -ItemType Directory -Force | Out-Null
+# Проверяем, было ли успешно выполнено подключение к серверу WSUS
+if ($wsus -ne $null) {
+    # Создаем каталог для журнала выполнения
+    $logDirectory = "D:\systemWSUS"
+    New-Item -Path $logDirectory -ItemType Directory -Force | Out-Null
 
-# Задаем путь к файлу журнала выполнения
-$logPath = Join-Path -Path $logDirectory -ChildPath "WSUS_Cleanup_Log_$(Get-Date -Format 'yyyyMMdd_HHmmss').txt"
+    # Задаем путь к файлу журнала выполнения
+    $logPath = Join-Path -Path $logDirectory -ChildPath "WSUS_Cleanup_Log_$(Get-Date -Format 'yyyyMMdd_HHmmss').txt"
 
-# Направляем вывод в файл журнала выполнения
-Start-Transcript -Path $logPath
+    # Направляем вывод в файл журнала выполнения
+    Start-Transcript -Path $logPath
 
-# Удаляем неиспользуемые обновления из базы данных WSUS
-Write-Host "Удаление неиспользуемых обновлений из базы данных WSUS..."
-$wsus.CleanupObsoleteUpdates()
+    # Удаляем неиспользуемые обновления из базы данных WSUS
+    Write-Host "Удаление неиспользуемых обновлений из базы данных WSUS..."
+    $wsus.CleanupObsoleteUpdates()
 
-# Удаляем неиспользуемые компьютеры из базы данных WSUS
-Write-Host "Удаление неиспользуемых компьютеров из базы данных WSUS..."
-$wsus.CleanupObsoleteComputers()
+    # Удаляем неиспользуемые компьютеры из базы данных WSUS
+    Write-Host "Удаление неиспользуемых компьютеров из базы данных WSUS..."
+    $wsus.CleanupObsoleteComputers()
 
-# Оптимизируем базу данных WSUS
-Write-Host "Оптимизация базы данных WSUS..."
-$wsus.Runspace.PerformingCleanup = $true
-$wsus.Runspace.PerformCleanup()
-$wsus.Runspace.PerformingCleanup = $false
+    # Оптимизируем базу данных WSUS
+    Write-Host "Оптимизация базы данных WSUS..."
+    $wsus.Runspace.PerformingCleanup = $true
+    $wsus.Runspace.PerformCleanup()
+    $wsus.Runspace.PerformingCleanup = $false
 
-Write-Host "Очистка базы данных WSUS завершена."
+    Write-Host "Очистка базы данных WSUS завершена."
 
-# Останавливаем запись в файл журнала выполнения
-Stop-Transcript
+    # Останавливаем запись в файл журнала выполнения
+    Stop-Transcript
+} else {
+    Write-Host "Ошибка: не удалось подключиться к серверу WSUS с именем '$wsusServer'. Проверьте правильность имени сервера WSUS."
+}
